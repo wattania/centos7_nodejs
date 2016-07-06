26af0bfbb29b487da7d5be6928f8b14a3d1c3d2e
Wattana Inthaphong <wattaint@gmail.com>
2016-07-06 15:40:12 +0700

    add new version

---
+++ docker/node4.4.3/docker-build.sh
@@ -0,0 +1,1 @@
+docker build -t registry.angstrom.co.th/centos7_nodejs:4.4.3 .
---
+++ docker/node4.4.5/docker-build.sh
@@ -0,0 +1,2 @@
+docker build -t registry.angstrom.co.th/centos7_nodejs:4.4.5 --no-cache .
+docker push registry.angstrom.co.th/centos7_nodejs:4.4.5
