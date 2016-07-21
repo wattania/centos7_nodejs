faed12e71f5b013f78b9e19534197680b48c504a
Wattana Inthaphong <wattaint@gmail.com>
2016-07-21 22:57:40 +0700

    add node 4.4.7

--- docker/node4.4.7/docker-build.sh
+++ docker/node4.4.7/docker-build.sh
@@ -1,2 +1,2 @@
-docker build -t registry.angstrom.co.th/centos7_nodejs:4.4.7 --no-cache .
-docker push registry.angstrom.co.th/centos7_nodejs:4.4.7
+docker build -t registry.angstrom.co.th:8443/centos7_nodejs:4.4.7 --no-cache .
+docker push registry.angstrom.co.th:8443/centos7_nodejs:4.4.7
